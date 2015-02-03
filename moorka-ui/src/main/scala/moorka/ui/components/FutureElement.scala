524a02db4164489266bdcebc99987a4e3d2d3955
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2015-02-03 14:53:56 +0300

    Future failure reporting when use implicit FutureElement

--- moorka-ui/src/main/scala/moorka/ui/components/FutureElement.scala
+++ moorka-ui/src/main/scala/moorka/ui/components/FutureElement.scala
@@ -4,6 +4,7 @@ import moorka.ui.element.{ElementBase, ElementExtension}
 import moorka.ui.components.html.div
 import moorka.rx._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 /**
  * @author Aleksey Fomkin <aleksey.fomkin@gmail.com>
@@ -13,12 +14,15 @@ class FutureElement(f: Future[ElementBase])
   val tempEl = div()
   def start(target: ElementBase): Unit = {
     target.ref.appendChild(tempEl.ref)
-    f foreach { newEl =>
-      target.ref.replaceChild(
-        newEl.ref, 
-        tempEl.ref
-      )
-      tempEl.kill()
+    f onComplete {
+      case Success(newEl) ⇒
+        target.ref.replaceChild(
+          newEl.ref,
+          tempEl.ref
+        )
+        tempEl.kill()
+      case Failure(ex) ⇒ 
+        throw ex
     }
   }
 }
